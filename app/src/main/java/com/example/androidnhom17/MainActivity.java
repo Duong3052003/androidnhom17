350c50c354faca10eb8bc6db9ce68c853b99faf3
Duong3052003 <txd3052003@gmail.com>
2023-09-29 12:35:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidnhom17/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidnhom17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
